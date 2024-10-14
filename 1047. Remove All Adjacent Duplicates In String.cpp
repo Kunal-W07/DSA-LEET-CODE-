45c250cb1e6acb2b25f36092ef1860f7395fa185
kunal_07 <165695901+Kunal-W07@users.noreply.github.com>
2024-10-14 19:50:49 +0530

    Update 1047. Remove All Adjacent Duplicates In String.cpp

--- 1047. Remove All Adjacent Duplicates In String.cpp
+++ 1047. Remove All Adjacent Duplicates In String.cpp
@@ -1,22 +1,23 @@
-class Solution {
+cclass Solution {
 public:
     string removeDuplicates(string s) {
         string ans = "";
         int n = s.length();
 
         for(int i=0; i<n; i++){
-            char current = s[i];
+            char currentstr = s[i];
+            //step 1 if new string is empty then insert
             if(ans.empty()){
-                ans.push_back(current);
+                ans.push_back(currentstr);
             }
-            else if(current==ans.back()){
-                ans.pop_back();
-            }
-            else if(current!=ans.back()){
-                ans.push_back(current);
-            }
+            //step 2 if it is not empty and right most elementis same the remove it
+             else if(currentstr == ans.back()){
+                 ans.pop_back();
+             }
+             else if(currentstr != ans.back()){
+                ans.push_back(currentstr);
+             }
         }
         return ans;
-
     }
 };
